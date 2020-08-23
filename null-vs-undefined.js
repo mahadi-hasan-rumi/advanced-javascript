e5c41fc599583eca34a9ba3395b6b17fcd251501
mahadi-hasan-rumi <mahadi528@gmail.com>
2020-08-23 12:44:51 +0600

    null vs undefined cleared

---
+++ null-vs-undefined.js
@@ -0,0 +1,26 @@
+// how you face undefined!
+
+let pakhi;
+console.log(pakhi)
+
+function add(num1, num2){
+    console.log(num1 + num2)
+}
+const result = add (12, 13)
+console.log(result)
+
+function add(num1, num2){
+    console.log(num1 + num2)
+    return;
+}
+const result = add (12, 13)
+console.log(result)
+
+const premik = {name: 'fotka', phone: 1325489}
+console.log(premik.gf)
+
+const value = undefined;
+console.log(value);
+
+let array = [2, 5, 7]
+console.log(array[5])
